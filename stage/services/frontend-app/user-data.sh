3b9f5d0febcfe58ecd0d773d55c6f56ad29d7eff
Ubuntu Git <ubuntu_git@example.com>
2020-03-03 13:47:12 +0300

    add stage tf files

---
+++ stage/services/frontend-app/user-data.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+yum update -y
+mkdir busybox_app && cd busybox_app
+wget https://busybox.net/downloads/binaries/1.28.1-defconfig-multiarch/busybox-x86_64
+mv busybox-x86_64 busybox
+chmod +x busybox
+echo "Hello, World from $(hostname)" > index.html
+nohup ./busybox httpd -f -p ${var.server_port} &
