b2dd39b4d74c54f2f19a27ba8df78125e4286eae
ghXplorer <57403493+ghXplorer@users.noreply.github.com>
2020-04-29 22:17:50 +0300

    Update main.tf

--- stage/services/frontend-app/main.tf
+++ stage/services/frontend-app/main.tf
@@ -174,12 +174,10 @@ resource "aws_lb_listener_rule" "asg" {
 
 terraform {
   backend "s3" {
-    # Replace this with your bucket name!
     bucket         = "terraform-sample-project-02032020"
     key            = "stage/services/frontend-app/terraform.tfstate"
     region         = "eu-central-1"
 
-    # Replace this with your DynamoDB table name!
     dynamodb_table = "terraform-sample-project-locks"
     encrypt        = true
   }
